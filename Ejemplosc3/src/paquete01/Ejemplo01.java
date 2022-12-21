49fc6479377246f8eef8401279de5df52a72a124
Fernando <fernandojosuem.15@gmail.com>
2022-12-21 09:53:38 -0500

    Trabajo

--- Ejemplosc3/src/paquete01/Ejemplo01.java
+++ Ejemplosc3/src/paquete01/Ejemplo01.java
@@ -22,7 +22,8 @@ public class Ejemplo01 {
         
         // forma 2
         int[][] arreglo2 = {{10, 20, 30}, {100, 200, 300}};
-        
+        //cadena
+        String[][] arreglocadena = {{"Ana","Jose","Pedro"
         // forma 3
         int[][] arreglo3 = new int[3][];
         arreglo3[0] = new int[2];
